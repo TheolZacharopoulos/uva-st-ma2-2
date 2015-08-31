ab4d60ffec837013adff19314a58b690151d77fa
guidouva <guidouva@users.noreply.github.com>
2015-08-31 20:13:37 +0200

    guard notation with where clause for Luhn

--- assignment-1/guido/Luhn.hs
+++ assignment-1/guido/Luhn.hs
@@ -1,11 +1,12 @@
 module Luhn (main) where
 
 listify :: Integer -> [Integer]
-listify x = (x `mod` 10) : tail
-  where tail = if x == x `mod` 10 then [] else listify (x `div` 10)
+listify x | x == xmod10 = [x]
+          | otherwise   = xmod10 : listify xdiv10
+  where (xdiv10, xmod10) = x `divMod` 10
 
 luhn :: Integer -> Bool
-luhn = (== 0) . (`mod` 10) . sum . (concat . map listify) . zipWith (*) (cycle [1,2]) . listify
+luhn = (== 0) . (`mod` 10) . sum . concat . map listify . zipWith (*) (cycle [1,2]) . listify
 
 main :: IO ()
 main = putStrLn $ show $ luhn 378282246310005
