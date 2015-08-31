7f21ca20e4d4b83ddc81e3d4981b7ca2a12d6975
Jelle van Assema <jelle.van.assema@gmail.com>
2015-08-31 16:08:59 +0200

    creditcard check v2

--- assignment-1/lab1_jelle.hs
+++ assignment-1/lab1_jelle.hs
@@ -1,15 +1,23 @@
 module Main (main) where
 
+compound_digits :: Integer -> Integer
+compound_digits 0 = 0
+compound_digits n = (compound_digits (n `div` 10)) + rem n 10  
+
+
 add_and_multiply :: Integer -> Integer
 add_and_multiply 0 = 0
-add_and_multiply n = (add (n `div` 10)) + (rem n 10) * 2
+add_and_multiply n = (add (n `div` 10)) + compound_digits ((rem n 10) * 2)
+
 
 add :: Integer -> Integer
 add 0 = 0
 add n = add_and_multiply (n `div` 10) + rem n 10
 
+
 luhn :: Integer -> Bool
 luhn n = (mod (add n) 10) == 0
 
+
 main :: IO ()
-main = putStrLn $ show $ luhn 505
+main = putStrLn $ show $ luhn 378282246310005
