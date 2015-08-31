62ecebfdebb657737551a55073e8a17e6e5e7fc8
guidouva <guidouva@users.noreply.github.com>
2015-08-31 17:55:11 +0200

    CSI

--- assignment-1/guido/CSI.hs
+++ assignment-1/guido/CSI.hs
@@ -1,3 +1,9 @@
 module CSI (main) where
 
+data Boy = Matthew | Peter | Jack | Arnold | Carl 
+    deriving (Eq,Show)
+
+boys :: [Boy]
+boys = [Matthew, Peter, Jack, Arnold, Carl]
+
 main = putStrLn "Hello, world"
