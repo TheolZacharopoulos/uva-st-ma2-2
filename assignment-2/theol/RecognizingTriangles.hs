5882f9c8ba4fc12ec8803db55817abe48830f601
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-09-07 14:44:30 +0200

    Recognizing Triangles theologos

---
+++ assignment-2/theol/RecognizingTriangles.hs
@@ -0,0 +1,16 @@
+module RecognizingTriangles where
+
+import Data.List
+
+data Shape = NoTriangle | Equilateral | Isosceles  | Rectangular | Other deriving (Eq, Show)
+
+triangleUsorted :: [Integer] -> Shape
+triangleUsorted [a,b,c] | (a > 0) && (b > 0) && (c > 0) = NoTriangle 
+                        | a >= b + c                    = NoTriangle 
+                        | b == c                        = Equilateral 
+                        | (a == b) && (b == a)          = Isosceles
+                        | (a^2 + b^2) == c^2            = Rectangular
+                        | otherwise                     = Other
+
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c = triangleUsorted (sort [a,b,c])
