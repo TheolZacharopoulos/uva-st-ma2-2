a4998008814decd9f76661a0348e82b11e910ab4
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-09-07 20:07:17 +0200

    Fix Triangles according to the tests

--- assignment-2/Theol/RecognizingTriangles.hs
+++ assignment-2/Theol/RecognizingTriangles.hs
@@ -1,16 +1,14 @@
 module RecognizingTriangles where
-
 import Data.List
 
 data Shape = NoTriangle | Equilateral | Isosceles  | Rectangular | Other deriving (Eq, Show)
 
-triangleSorted :: [Integer] -> Shape
-triangleSorted [a,b,c] | (a <= 0) || (b <= 0) || (c <= 0) = NoTriangle 
-                       | a >= b + c                       = NoTriangle 
-                       | b == c                           = Equilateral 
-                       | (a == b) && (b == a)             = Isosceles
-                       | (a^2 + b^2) == c^2               = Rectangular
-                       | otherwise                        = Other
-
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle a b c = triangleSorted $ reverse $ sort [a,b,c]
+triangle a b c | (a <= 0) || (b <= 0) || (c <= 0) = NoTriangle 
+               | sorted_a >= sorted_b + sorted_c  = NoTriangle 
+               | (a == b) && (b == c)             = Equilateral 
+               | (a == b) || (b == c)             = Isosceles
+               | (a^2 + b^2) == c^2               = Rectangular
+               | otherwise                        = Other
+    where 
+        [sorted_a, sorted_b, sorted_c] = reverse $ sort [a,b,c]
