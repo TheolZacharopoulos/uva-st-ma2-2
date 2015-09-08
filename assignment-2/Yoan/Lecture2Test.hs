80876033b7cc3c965b43809e0cc9206615e3f41e
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2015-09-08 14:35:18 +0200

    Updated tests suite

--- assignment-2/Yoan/Lecture2Test.hs
+++ assignment-2/Yoan/Lecture2Test.hs
@@ -1,7 +1,10 @@
 module Lecture2Test where
 
+numTests :: Int
+numTests = 10000
+
 -- Generalized test framework from lecture 2
-testR :: Show a
+testR :: (Show a,  Show b)
       => Int
       -> Int
       -> (a -> b)
@@ -11,13 +14,14 @@ testR :: Show a
 testR k n f r gen | k == n    = print (show n ++ " tests passed")
                   | otherwise = do
     h <- gen
-    if r h (f h) then do
-        print ("pass on: " ++ show h)
+    h' <- return $ f h
+    if r h h' then do
+        print ("pass on: (" ++ show h ++ ", " ++ show h' ++ ")")
         testR (k+1) n f r gen
-    else error ("failed test on: " ++ show h)
+    else error ("failed test on: (" ++ show h ++ ", " ++ show h' ++ ")")
 
-testPost :: Show a => (a -> b) -> (b -> Bool) -> IO a -> IO ()
-testPost f p = testR 1 100 f (\_ -> p)
+testPost :: (Show a, Show b) => (a -> b) -> (b -> Bool) -> IO a -> IO ()
+testPost f p = testR 1 numTests f (\_ -> p)
 
-testRel :: Show a => (a-> b) -> (a -> b -> Bool) -> IO a -> IO ()
-testRel = testR 1 100
+testRel :: (Show a, Show b) => (a-> b) -> (a -> b -> Bool) -> IO a -> IO ()
+testRel = testR 1 numTests
