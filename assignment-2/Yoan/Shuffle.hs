ca8721273dc14da8dfd7d228d742fe019a25e897
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2015-09-08 14:22:55 +0200

    Added permutation test cases + updated gitignore

---
+++ assignment-2/Yoan/Shuffle.hs
@@ -0,0 +1,20 @@
+module Shuffle where
+
+import System.Random
+import Data.Array.IO
+import Control.Monad
+
+-- source: https://wiki.haskell.org/Random_shuffle
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs
