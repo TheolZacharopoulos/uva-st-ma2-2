47d2f0c59b936387f5774ae24f94f60c6084608d
guidouva <guidouva@users.noreply.github.com>
2015-09-09 13:09:28 +0200

    even clearer error messages

--- assignment-2/Lecture2Test.hs
+++ assignment-2/Lecture2Test.hs
@@ -13,12 +13,12 @@ testR :: (Show a,  Show b)
       -> IO ()
 testR k n f r gen | k == n    = print (show n ++ " tests passed")
                   | otherwise = do
-    h <- gen
+    h  <- gen
     h' <- return $ f h
     if r h h' then do
-        print ("pass on: (" ++ show h ++ ", " ++ show h' ++ ")")
+        print ("pass (actual input: `" ++ show h ++ "`; actual output: `" ++ show h' ++ "`)")
         testR (k+1) n f r gen
-    else error ("failed test on: (" ++ show h ++ ", " ++ show h' ++ ")")
+    else error ("fail (actual input: `" ++ show h ++ "`; actual output: `" ++ show h' ++ "`)")
 
 testPost :: (Show a, Show b) => (a -> b) -> (b -> Bool) -> IO a -> IO ()
 testPost f p = testR 1 numTests f (\_ -> p)
