90e4c6739b4b2d3bc73ea74876e0834f9bf40ee5
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2015-09-15 13:58:14 +0200

    Added FormulaGenerator and used in TestParse

--- assignment-3/TestParse.hs
+++ assignment-3/TestParse.hs
@@ -6,65 +6,27 @@ import Data.List
 import Lecture3
 import Lab3_1
 import Lecture2Test
+import FormulaGenerator
 
--- Preconditions: We do not have any preconditions
+-- Name range to be used when generating formulas
+formulaGeneratorRange :: (Name, Name)
+formulaGeneratorRange = (1, 15)
 
--- Postconditions:
--- Returns true if:
---      Originally generated formula and its stringified and then parsed version be equivalent
-
--- We define a maximum depth limit of formula generation
--- See generateForm function
-depthLimit :: Integer
-depthLimit = 10
-
--- Names are generated using a range of numbers.
-nameLimit :: Integer
-nameLimit = 10
-
--- Function for name generation
--- Uses nameLimit for range generation
-generateName :: IO Form
-generateName = do
-    name <- randomRIO(1, nameLimit)
-    return (Prop name)
-
--- getForm function resolves and returns formula
--- based on 3 parameters:
---    formUsed - integer value ranging from 1 to 5. Used to choose the right formular
---    a        - first applied formula
---    b        - second applied formula
-getForm :: Integer -> Form -> Form -> Form
-getForm formUsed a b = case formUsed of
-    1 -> Impl a b
-    2 -> Equiv a b
-    3 -> Neg b
-    4 -> Cnj [a, b]
-    5 -> Dsj [a, b]
-
--- Randomly generates new formulas. It is required a starting formula to be provided.
--- Furthermore, a depth counter is required for the recursion.
-generateForm :: Form -> Integer -> IO Form
-generateForm generated depth = do
-    a <- generateName
-    b <- generateName
-    formType <- randomRIO(1, 5)
-    if depth == depthLimit then return generated
-    else (generateForm (getForm formType a generated) (depth + 1))
+-- Random sub-formulas to be used
+randomSubForms :: Int
+randomSubForms = 10
 
 -- Helper function for uncurrying the ``parse`` function
 -- Uses an input formula. After that the formula is being stringified with ``show``
 -- Next step is the stringified version to be parsed.
 -- Finally we compare the original formula and the parsed one using equiv
 unCurriedParse :: Form -> Bool
-unCurriedParse form = equiv form actual
-    where actual = (head(parse (show form)))
+unCurriedParse expectedForm = equiv expectedForm actual
+    where actual = (head(parse (show expectedForm)))
 
--- Generates a random formula. Used as a parameterless wrapper over ``generateForm``
+-- Generates a random formula
 randomFormCase :: IO Form
-randomFormCase = do
-    startForm <- generateName
-    generateForm startForm 1
+randomFormCase = randomForm formulaGeneratorRange randomSubForms
 
 -- Initiates a set of parse tests using randomly generated formulas
 testParseCase :: IO ()
