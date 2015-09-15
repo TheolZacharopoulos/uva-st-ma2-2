3c0b8408c81c281bef7f21c65977e2f1e45ae40d
guidouva <guidouva@users.noreply.github.com>
2015-09-15 16:05:39 +0200

    cnfconvtest

--- assignment-3/CnfConvTest.hs
+++ assignment-3/CnfConvTest.hs
@@ -1,6 +1,6 @@
-module CnfConvTest
+module CnfConvTest where
 
-where
+import System.Random
 
 import Lecture3
 import Lab3_1
@@ -28,3 +28,39 @@ unCurriedIsCnfConv form = isCnf converted && equiv converted form
 testCnf :: IO ()
 testCnf =
     testPost unCurriedIsCnfConv (== True) randomFormCase
+
+testIsCnfGood :: IO ()
+testIsCnfGood =
+    testPost isCnf id randomGoodCnfCase
+
+testIsCnfBad :: IO ()
+testIsCnfBad =
+    testPost isCnf not randomBadCnfCase
+
+randomGoodCnfCase :: IO Form
+randomGoodCnfCase = do
+    i <- randomRIO (0, length knownCnfs - 1)
+    return $ knownCnfs !! i
+
+randomBadCnfCase :: IO Form
+randomBadCnfCase = do
+    goodCnf <- randomGoodCnfCase
+    n <- randomRIO (0,1) :: IO Int
+    return $ case n of
+        0 -> Cnj [Cnj [goodCnf]]
+        1 -> Dsj [Cnj [goodCnf]]
+
+knownCnfs :: [Form]
+knownCnfs =
+    [Prop 1
+    ,Neg (Prop 1)
+    ,Dsj []
+    ,Dsj [Prop 1]
+    ,Dsj [Prop 1, Neg (Prop 2)]
+    ,Cnj []
+    ,Cnj [Prop 1]
+    ,Cnj [Prop 1, Neg (Prop 2)]
+    ,Cnj [Prop 1, Neg (Prop 2), Dsj []]
+    ,Cnj [Prop 1, Neg (Prop 2), Dsj [Prop 3]]
+    ,Cnj [Prop 1, Neg (Prop 2), Dsj [Prop 3, Neg (Prop 4)]]
+    ]
