4d74ae0921f565be410fb7d5929c0f6ae9bb86cd
guidouva <guidouva@users.noreply.github.com>
2015-09-15 15:26:51 +0200

    modified some stuff

--- assignment-3/Lab3_1.hs
+++ assignment-3/Lab3_1.hs
@@ -1,15 +1,33 @@
 module Lab3_1 where
-
 import Lecture3
 
+-- Preconditions: None
+-- Postconditions:
+-- Returns True iff:
+-- 		There exists no input, such that Form returns true.
 contradiction :: Form -> Bool
-contradiction f = (not.satisfiable) f
-    
+contradiction = not . satisfiable
+
+-- Preconditions: None
+-- Postconditions:
+-- Returns True iff:
+-- 		For any input Form returns true
 tautology :: Form -> Bool
-tautology f = all (\v -> evl v f) (allVals f)
+tautology f = all (\ v -> evl v f) (allVals f)
 
+-- Preconditions: None
+-- Postconditions:
+-- Returns True iff:
+-- 		For any input for both Forms:
+--			(Form1 returns False) or (Form2 returns True)
 entails :: Form -> Form -> Bool
-entails f g = all (\v -> if evl v f then evl v g else True) (allVals $ Impl f g)
+entails f g = all (\ v -> if evl v f then evl v g else True) (allVals (Impl f g))
 
+-- Preconditions: None
+-- Preconditions: None
+-- Postconditions:
+-- Returns True iff:
+-- 		For any input for both Forms:
+--			The truth value of both forms are the same.
 equiv :: Form -> Form -> Bool
-equiv f g = all (\v -> (evl v f) == (evl v g)) (allVals $ Equiv f g)
+equiv f g = all (\v -> (evl v f) == (evl v g)) (allVals (Equiv f g))
--- assignment-3/Lab3_1_tests.hs
+++ assignment-3/Lab3_1_tests.hs
@@ -1,22 +1,32 @@
 module Lab3_1_tests where
-import Lecture3
 import Lab3_1
 
 tautologyForm = (Dsj [p, (Neg p)])
 contradictionForm = (Cnj [p, (Neg p)])
 
+-- Tests contradiction, by entering all three cases and checking the output:
+--      Satisfiable (one that is not also a Tautology) (<- should return False),
+--      Contradiction (<- should return True) and
+--      Tautology (<- should return False)
 testContradiction :: Bool
 testContradiction = 
     not (contradiction p) &&
     not (contradiction tautologyForm) &&
     contradiction contradictionForm
 
+-- Tests tautology, by entering all three cases and checking the output:
+--      Satisfiable (one that is not also a Tautology) (<- should return False),
+--      Contradiction (<- should return False) and
+--      Tautology (<- should return True)
 testTautology :: Bool
 testTautology =
     not (tautology p) &&
     tautology tautologyForm &&
     not (tautology contradictionForm)
 
+-- Tests entails by entering its entire truth table and checking the output.
+-- Note! entails takes in formulas, but we can use tautologies and 
+-- contradictions to replicate simple truth values.
 testEntails :: Bool
 testEntails =
     (entails tautologyForm tautologyForm) &&
@@ -24,16 +34,12 @@ testEntails =
     (entails contradictionForm tautologyForm) &&
     (entails contradictionForm contradictionForm)
 
+-- Tests equiv by entering its entire truth table and checking the output.
+-- Note! equiv takes in formulas, but we can use tautologies and 
+-- contradictions to replicate simple truth values.
 testEquiv :: Bool
 testEquiv =
     (equiv tautologyForm tautologyForm) &&
     not (equiv tautologyForm contradictionForm) &&
     not (equiv contradictionForm tautologyForm) &&
     (equiv contradictionForm contradictionForm)
-
-testAllLab3_1 :: IO ()
-testAllLab3_1 = do
-    print ("Testing contradiction, tautology, entails, equivalence... " ++ testResult)
-    where
-        allTests = all (== True) [testContradiction, testTautology, testTautology, testEntails, testEquiv]
-        testResult = if allTests then "Passed!" else "Failed!"
