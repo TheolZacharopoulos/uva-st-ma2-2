c3f5ae4495f8592591bba790be8e4d14fa95b730
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-09-20 13:36:30 +0200

    Refactor quickCheck form generator

--- assignment-3/Theol/QCFormulaGenerator.hs
+++ assignment-3/Theol/QCFormulaGenerator.hs
@@ -1,4 +1,5 @@
-module QCFormulaGenerator(main) where
+-- A QuickCheck Formula Generator.
+module QCFormulaGenerator (main) where
 
 import Test.QuickCheck
 import Data.List
@@ -6,17 +7,20 @@ import Control.Monad
 
 import Lecture3
 
--- Preconditions: We do not have any preconditions
+-- Options
+propNumber = 4
+negationDepth = 3
 
+-- Preconditions: We do not have any preconditions
 -- Postconditions:
 -- Returns true iff:
 --      Originally generated formula and its stringified and then parsed version be equivalent
 prop_parsed :: Form -> Bool
-prop_parsed f =
-    let formula = show f in
-        show (head $ parse formula) == formula
+prop_parsed form =
+    let formulaStr = show form in
+        show (head $ parse formulaStr) == formulaStr
 
--- As mention in (http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html),
+-- As mentioned in (http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html),
 -- There is always a risk that a recursive generator like this may fail to terminate,
 -- or produce very large results. To avoid this, recursive generators should always use the size control mechanism.
 instance Arbitrary Form where
@@ -28,38 +32,34 @@ sizedRandomForm = sized rndForm
 rndForm :: Integral a => a -> Gen Form
 rndForm 0 =
     oneof [
-        liftM Prop (resize (3) arbitrary),
-        liftM Neg  (resize (3) arbitrary)
+        liftM Prop (elements [1..propNumber]),
+        liftM Neg  (resize (negationDepth) arbitrary)
       ]
 
 rndForm n | n > 0 =
-    frequency
-        [ (10, liftM  Prop  (resize (3) arbitrary))
-        , (10, liftM  Neg   (resize (3) arbitrary))
-        , (7,  liftM  Cnj   subforms)
-        , (7,  liftM  Dsj   subforms)
-        , (5,  liftM2 Impl  subform subform)
-        , (5,  liftM2 Equiv subform subform)]
+    oneof [
+        liftM  Prop  (elements [1..propNumber]),
+        liftM  Neg   (resize (negationDepth) arbitrary),
+        liftM  Cnj   (listOf subform),
+        liftM  Dsj   (listOf subform),
+        liftM2 Impl  subform subform,
+        liftM2 Equiv subform subform
+      ]
     where
+        -- Generates a form and divides n to control the size of recursive depth.
         subform :: Gen Form
-        subform = rndForm (n `div` 2)
-
-        -- TODO: Refactor this.
-        subforms :: Gen [Form]
-        subforms = do
-            fs <- vectorOf 1 subform
-            return fs
+        subform = rndForm (n `div` 10)
 
 ---------------------------------
--- Test Options
+-- Test runner options
 options :: Args
-options = Args
-    { replay = Nothing
-    , maxSuccess = 100
-    , maxDiscardRatio = 100
-    , maxSize = 100
-    , chatty = True
-    }
+options = Args {
+    replay = Nothing,
+    maxSuccess = 100,
+    maxDiscardRatio = 100,
+    maxSize = 100,
+    chatty = True
+}
 
 runTests :: Args -> IO ()
 runTests args = do
