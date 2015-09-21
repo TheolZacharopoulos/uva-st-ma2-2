d860026782f3d8223fa42d376305d508d98e500d
guidouva <guidouva@users.noreply.github.com>
2015-09-21 14:47:09 +0200

    nontrivial implementations of difference and intersection

--- assignment-4/Guido/SetOps.hs
+++ assignment-4/Guido/SetOps.hs
@@ -3,18 +3,22 @@ module SetOps where
 import Data.List
 import SetOrd
 
--- card :: Set a -> Integer
--- card (Set xs) = length xs
+card :: Set a -> Int
+card (Set xs) = length xs
 
 intersectSet :: (Ord a) => Set a -> Set a -> Set a
-intersectSet (Set xs) (Set ys) = Set $ intersect xs ys
--- intersectSet small@(Set xs) large@(Set ys)
---    | card small > card large = intersectSet' ys xs
---    | otherwise               = intersectSet' xs ys 
---  where intersectSet' xs' ys' = Set $ foldr insert' emptySet xs'
---            where insert' x s@(Set zs) = if inSet x ys'
---                                         then insertSet x s
---                                         else s
+-- intersectSet (Set xs) (Set ys) = Set $ intersect xs ys
+intersectSet small@(Set xs) large@(Set ys)
+   | card small > card large = intersectSet' ys small
+   | otherwise               = intersectSet' xs large 
+   where intersectSet' xs' s = foldr insert' emptySet xs'
+           where insert' x s' = if inSet x s
+                                then insertSet x s'
+                                else s'
 
 differenceSet :: (Ord a) => Set a -> Set a -> Set a
-differenceSet (Set xs) (Set ys) = Set $ xs \\ ys
+-- differenceSet (Set xs) (Set ys) = Set $ xs \\ ys
+differenceSet left@(Set xs) right = foldr insert' emptySet xs
+   where insert' x s = if not (inSet x right)
+                       then insertSet x s
+                       else s
