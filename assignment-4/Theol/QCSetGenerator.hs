903af5ae53d88c761530d0d41eda288f98b197f8
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-09-21 15:49:51 +0200

    Rimplementation of quickCheck generator, without using the list2set

--- assignment-4/Theol/QCSetGenerator.hs
+++ assignment-4/Theol/QCSetGenerator.hs
@@ -3,11 +3,24 @@ module QCSetGenerator where
 import SetOrd
 import Test.QuickCheck
 import Control.Monad (replicateM)
+import Data.List
 
 -- Generic arbitrary Set generator.
+-- Without using list2set
 instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
-    arbitrary = 
-        sized $ \size -> do 
-            len <- choose (0, size)
-            values <- replicateM len arbitrary 
-            return $ list2set values
+    arbitrary = do 
+            values <- (suchThat orderedList isUniqueList)
+            return $ Set(values)
+            where isUniqueList xs = (nub xs == xs) 
+
+-- Generic arbitrary Set generator.
+-- Using list2set
+--instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
+--    arbitrary = 
+--        sized $ \size -> do 
+--            len <- choose (0, size)
+--            values <- replicateM len arbitrary 
+--            return $ list2set values
+
+-- Generate random Int Sets
+genIntSetQC = sample (arbitrary :: Gen (Set Int))
