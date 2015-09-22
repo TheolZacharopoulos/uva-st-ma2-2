c60d351049742d067726d2a5f556341bd64c1806
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-09-22 15:47:58 +0200

    Props in rels

--- assignment-4/RelationsTest.hs
+++ assignment-4/RelationsTest.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module RelationsTest where
 
 {- We use QuickCheck to test some invariants of the relations. 
@@ -8,16 +9,17 @@ module RelationsTest where
  -}
 
 import Test.QuickCheck
+import Test.QuickCheck.All
 import Data.List
 import Relations
 
-newtype TRel a = TRel (Rel a)
+newtype TestRel a = TestRel (Rel a)
     deriving (Show)
 
-instance (Arbitrary a, Ord a) => Arbitrary (TRel a) where
+instance (Arbitrary a, Ord a) => Arbitrary (TestRel a) where
     arbitrary = do
         values <- (suchThat orderedList isUniqueList)
-        return $ TRel values
+        return $ TestRel values
         where
             isUniqueList xs = (nub xs == xs)
 
@@ -25,46 +27,46 @@ isRel :: Rel Int -> Bool
 isRel r = length r == length (nub r) && sort r == r
 
 -- (inverseRel r) is a valid relation
-prop_isRel_inverseRel :: TRel Int -> Bool
-prop_isRel_inverseRel (TRel r) = isRel (inverseRel r)
+prop_isRel_inverseRel :: TestRel Int -> Bool
+prop_isRel_inverseRel (TestRel r) = isRel (inverseRel r)
 
 -- (inverseRel r) contains all the inverted pairs of r.
-prop_hasAll_inverseRel :: Rel Int -> Property
-prop_hasAll_inverseRel r = isRel r ==>
+prop_hasAll_inverseRel :: TestRel Int -> Bool
+prop_hasAll_inverseRel (TestRel r) = 
     all (\(x,y) -> (y,x) `elem` r') r
     where r' = inverseRel r
 
 -- r contains all the inverted pairs of (inverseRel r).
-prop_hasOnly_inverseRel :: Rel Int -> Property
-prop_hasOnly_inverseRel r = isRel r ==>
+prop_hasOnly_inverseRel :: TestRel Int -> Bool
+prop_hasOnly_inverseRel (TestRel r) = 
     all (\(x,y) -> (y,x) `elem` r) (inverseRel r)
 
 -- inverseRel is an `involution` 
-prop_involution_inverseRel :: Rel Int -> Property
-prop_involution_inverseRel r = isRel r ==>
+prop_involution_inverseRel :: TestRel Int -> Bool
+prop_involution_inverseRel (TestRel r) =
     (inverseRel.inverseRel) r == r
 
 -- (unionRel r) is a valid relation
-prop_isRel_unionRel :: TRel Int -> TRel Int -> Bool
-prop_isRel_unionRel (TRel r) (TRel s) = isRel (unionRel r s)
+prop_isRel_unionRel :: TestRel Int -> TestRel Int -> Bool
+prop_isRel_unionRel (TestRel r) (TestRel s) = isRel (unionRel r s)
 
 -- (unionRel r s) contains all the elements of r and s.
-prop_hasAll_unionRel :: Rel Int -> Rel Int -> Property
-prop_hasAll_unionRel r s = isRel r && isRel s ==>
+prop_hasAll_unionRel :: TestRel Int -> TestRel Int -> Bool
+prop_hasAll_unionRel (TestRel r) (TestRel s) =
     all (flip elem u) (r ++ s)
     where u = unionRel r s
 
 -- (unionRel r s) contains only elements in either r or s.
-prop_hasOnly_unionRel :: Rel Int -> Rel Int -> Property
-prop_hasOnly_unionRel r s = isRel r && isRel s ==>
+prop_hasOnly_unionRel :: TestRel Int -> TestRel Int -> Bool
+prop_hasOnly_unionRel (TestRel r) (TestRel s) = 
     all (\e -> e `elem` r || e `elem` s) (unionRel r s)
 
 -- Invariants on (symClos r)
 -- 1. is a valid relation
 -- 2. contains all the elements that are in either r or (inverseRel r)
 -- 3. contains only the elements that are in either r or (inverseRel r)
-prop_hasOnlyOriginalAndInverse_symClos :: Rel Int -> Property
-prop_hasOnlyOriginalAndInverse_symClos r = isRel r ==>
+prop_hasOnlyOriginalAndInverse_symClos :: TestRel Int -> Bool
+prop_hasOnlyOriginalAndInverse_symClos (TestRel r) =
     isRel r'
     &&
     all (\e -> e `elem` r || e `elem` ri) r'
@@ -77,8 +79,8 @@ prop_hasOnlyOriginalAndInverse_symClos r = isRel r ==>
 -- 1. is a valid relation
 -- 2. contains all the elements that are in either r or (r @@ r)
 -- 3. contains only the elements that are in either r or (r @@ r)
-prop_hasOnlyOriginalAndTransitive_trClos :: Rel Int -> Property
-prop_hasOnlyOriginalAndTransitive_trClos r = isRel r ==>
+prop_hasOnlyOriginalAndTransitive_trClos :: TestRel Int -> Bool
+prop_hasOnlyOriginalAndTransitive_trClos (TestRel r) =
     isRel r'
     &&
     all (\e -> e `elem` r || e `elem` rt) r'
@@ -87,17 +89,5 @@ prop_hasOnlyOriginalAndTransitive_trClos r = isRel r ==>
     where r' = trClos r
           rt = r @@ r
 
-runTests = do
-    sequence_ $ map (quickCheckWith args)
-        [prop_isRel_inverseRel
-        ]
-    sequence_ $ map (quickCheckWith args)
-        [prop_isRel_unionRel
-        ]
-    where args = Args {
-        replay = Nothing,
-        maxSuccess = 100,
-        maxDiscardRatio = 100,
-        maxSize = 100,
-        chatty = True
-    }
+return []
+runTests = $quickCheckAll
