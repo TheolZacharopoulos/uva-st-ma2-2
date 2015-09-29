d15173dec3a99514890efd21c3cd5c2d65c3a522
guidouva <guidouva@users.noreply.github.com>
2015-09-29 11:50:46 +0200

    profile scripts

--- assignment-5/profile.sh
+++ assignment-5/profile.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env sh
+cp -v Lecture5.prof Lecture5.prof.old
+cp -v Lecture5_2.prof Lecture5_2.prof.old
+
 ghc -prof -auto-all -o Lecture5 Lecture5.hs
 ghc -prof -auto-all -o Lecture5_2 Lecture5_2.hs
+rm -v *.hi *.hs *.o
 
 ./Lecture5 +RTS -p
 ./Lecture5_2 +RTS -p
