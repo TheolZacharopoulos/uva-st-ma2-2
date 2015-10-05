2a6a91a75e122aee4326f038e3c3678c06fe4084
guidouva <guidouva@users.noreply.github.com>
2015-10-05 20:53:18 +0200

    variable name readability and make sure result is mod m

--- assignment-6/Guido/ExM.hs
+++ assignment-6/Guido/ExM.hs
@@ -3,9 +3,9 @@ module ExM where
 import Lecture6
 
 exM :: Integer -> Integer -> Integer -> Integer
-exM _ 0 _ = 1
+exM _ 0 m = 1 `mod` m
 exM b e m = exMsq 1 (b `mod` m)
-    where exMsq e' b' = let e'2 = e'*2
-                        in  if e'2 <= e
-                            then exMsq e'2 (b'*b' `mod` m)
-                            else b' * (b^(e-e') `mod` m)
+    where exMsq e' b' = let e2' = e'*2
+                        in  if e2' <= e
+                            then exMsq e2' (b'*b' `mod` m)
+                            else b' * b^(e-e') `mod` m
