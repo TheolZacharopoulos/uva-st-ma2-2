864bdc073ed90bf740cb024633099a3da935366e
guidouva <guidouva@users.noreply.github.com>
2015-10-05 22:16:35 +0200

    * New version of ExM without recursion * Scripts to benchmark `expM` vs `exM`.`

--- assignment-6/Guido/ExM.hs
+++ assignment-6/Guido/ExM.hs
@@ -4,8 +4,7 @@ import Lecture6
 
 exM :: Integer -> Integer -> Integer -> Integer
 exM _ 0 m = 1 `mod` m
-exM b e m = exMsq 1 (b `mod` m)
-    where exMsq e' b' = let e2' = e'*2
-                        in  if e2' <= e
-                            then exMsq e2' (b'*b' `mod` m)
-                            else b' * b^(e-e') `mod` m
+exM b e m = b' * b^(e-e') `mod` m
+    where (e',b') = until (\(x,_) -> x*2 > e)
+                          (\(x,y) -> (x*2,y^2 `mod` m))
+                          (1,b `mod` m)
---
+++ assignment-6/Guido/Lecture6_2_exM.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import ExM
+
+main = print $ ExM.exM (10^3) (10^8) 78
