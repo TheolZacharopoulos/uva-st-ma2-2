0a89d8231d1fe0cee135b150341966b13ef90365
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-10-06 16:12:03 +0200

    Refactor mersenne a bit

--- assignment-6/Theol/Mersenne.hs
+++ assignment-6/Theol/Mersenne.hs
@@ -7,11 +7,11 @@ import MillerRabin
 
 import Control.Monad
 
--- Get the precision and the primes range.
+-- Get the precision and the wanted mersenne number.
 get_mersenne k m = filterM (primeMR k) m_prime_list
 	where
-		m_prime_list = map (\p -> (2^p)-1) (take m primes)
+		m_prime_list = map (\p -> (2^p)-1) (take (m*10) primes) -- Todo: Make it unsafePerformIO ???
 
 mersenne n = do
-	mers <- (get_mersenne 2 100)
+	mers <- (get_mersenne 2 n)
 	return $ take n mers
