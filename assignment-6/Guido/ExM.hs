1d75202a42fe22f59475158017bbe65248e7465d
guidouva <guidouva@users.noreply.github.com>
2015-10-06 10:13:25 +0200

    remove unused import

--- assignment-6/Guido/ExM.hs
+++ assignment-6/Guido/ExM.hs
@@ -1,10 +1,8 @@
 module ExM where
 
-import Lecture6
-
 exM :: Integer -> Integer -> Integer -> Integer
 exM _ 0 m = 1 `mod` m
 exM b e m = b' * b^(e-e') `mod` m
     where (e',b') = until (\(x,_) -> x*2 > e)
-                          (\(x,y) -> (x*2,y^2 `mod` m))
+                          (\(x,y) -> (x*2,y*y `mod` m))
                           (1,b `mod` m)
