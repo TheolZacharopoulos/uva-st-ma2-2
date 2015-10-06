2d77faaa6394d92cc614ec7209c74a81b04b5315
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-10-06 11:26:42 +0200

    Add 2 styles of solution for fermat tests

--- assignment-6/Theol/Fermat.hs
+++ assignment-6/Theol/Fermat.hs
@@ -44,8 +44,20 @@ prime_tests_F k n = do
   as <- sequence $ fmap (\_-> randomRIO (1,n-1)) [1..k]
   return (all (\ a -> exM a (n-1) n == 1) as)
 
+-- A bit imperative style
 test_fermat k = do
-  composite_test <- (mapM (prime_tests_F k) (take 2000 composites))
-  let index = findIndex (== True) composite_test
-  return $ composites !! (getFromJust index)
-    where getFromJust = \ (Just v) -> v
+  composite_test <- (mapM (prime_tests_F k) comps)
+  let zipped = zip comps composite_test
+  let trueOccurences = filter (\x -> snd x == True) zipped
+  let firstTrue = fst (trueOccurences !! 1)
+  return firstTrue
+    where comps = (take 200000 composites)
+
+-- More functional style
+test_fermat_2 k = test_fermat_2' k composites
+  where 
+    test_fermat_2' k (c:cs) = do
+    test_res <- prime_tests_F k c
+    (case test_res of
+          True  -> return c
+          False -> (test_fermat_2' k cs))
